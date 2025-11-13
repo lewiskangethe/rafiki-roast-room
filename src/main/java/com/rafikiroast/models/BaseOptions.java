f72e9c4620ee36661c71aae0304df02a49d20a7b
lewiskangethe <lewiskkangethe@gmail.com>
2025-11-13 10:45:06 -0500

    classes commit - includes BaseOPtions and Proteins classes

--- src/main/java/com/rafikiroast/models/BaseOptions.java
+++ src/main/java/com/rafikiroast/models/BaseOptions.java
@@ -1,4 +1,30 @@
 package com.rafikiroast.models;
 
 public class BaseOptions {
+    private String name;
+    private String size;
+    private double basePrice;
+
+    public BaseOptions(String name, String size, double basePrice) {
+        this.name = name;
+        this.size = size;
+        this.basePrice = basePrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    @Override
+    public String toString() {
+        return size + " " + name + " - $" + String.format("%.2f", basePrice);
+    }
 }
--- src/main/java/com/rafikiroast/models/Proteins.java
+++ src/main/java/com/rafikiroast/models/Proteins.java
@@ -1,4 +1,24 @@
 package com.rafikiroast.models;
 
 public class Proteins {
+    private String name;
+    private double extraCost;
+
+    public Proteins(String name, double extraCost) {
+        this.name = name;
+        this.extraCost = extraCost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getExtraCost() {
+        return extraCost;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Extra: $" + String.format("%.2f", extraCost) + ")";
+    }
 }
